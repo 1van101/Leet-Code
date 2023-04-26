6e37ab5b8e02310f7917c574db5dd82516c642ac
IvanTsvetanov <ivan.iv.cv@gmail.com>
2023-04-26 15:37:03 +0300

    rearranged folder python

--- python/258_add_digits.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def addDigits(self, num: int) -> int:
-        while num >= 10:
-            num = sum([int(x) for x in str(num)])
-
-        return num
--- python/412_fizz_buzz.py
+++
@@ -1,8 +0,0 @@
-def fizzBuzz(n: int):
-    return [
-        'Fizz' if x % 3 == 0
-        else 'Buzz' if x % 5 == 0
-        else 'FizzBuzz' if x % 15 == 0
-        else x
-        for x in range(1, n + 1)
-    ]
