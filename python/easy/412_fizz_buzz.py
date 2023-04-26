57def2a829a8189ee1b83ab00bb3b21caac9aefe
IvanTsvetanov <ivan.iv.cv@gmail.com>
2023-04-26 16:11:54 +0300

    replaced

--- python/easy/412_fizz_buzz.py
+++ python/easy/412_fizz_buzz.py
@@ -1,8 +1,9 @@
-def fizzBuzz(n: int):
-    return [
-        'Fizz' if x % 3 == 0
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        return [
+        'FizzBuzz' if x % 15 == 0
+        else 'Fizz' if x % 3 == 0
         else 'Buzz' if x % 5 == 0
-        else 'FizzBuzz' if x % 15 == 0
-        else x
+        else str(x)
         for x in range(1, n + 1)
     ]
